1665e67f3db8f57ebd76e3d40865a469cc72490b
Karly Sindy <karlysindy@gmail.com>
2016-12-20 08:14:37 -0500

    rename files

---
+++ scroll_box_matplotlib.py
@@ -0,0 +1,29 @@
+#import dependencies 
+import numpy as np
+from matplotlib import animation, rc
+import matplotlib.pyplot as plt
+from IPython.display import HTML
+import matplotlib.patches as patches
+
+
+fig = plt.figure()
+ax = fig.add_subplot(111)
+ax.set_xlim(0, 10)
+ax.set_ylim(0, 10)
+
+patch = patches.Rectangle((0, 0), 0, 0,fc='y')
+
+
+def init():
+    ax.add_patch(patch)
+    return patch, 
+
+
+anim = animation.FuncAnimation(fig, animate,
+                               init_func=init,
+                               frames=len(x),
+                               interval=50,
+                               blit=True)
+
+HTML(anim.to_html5_video())
+
